93515bb016c4016502feb10150b02a3f60ac0e6a
Valentina Moretti <valentina.moretti@mail.polimi.it>
2022-04-10 20:28:12 +0200

    LobbyManager

--- src/main/java/it/polimi/ingsw/Controller/LobbyManager.java
+++ src/main/java/it/polimi/ingsw/Controller/LobbyManager.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class LobbyManager {
@@ -19,6 +21,7 @@ public class LobbyManager {
     private int lobbyPortNumber;
 
     public LobbyManager(int lobbyPortNumber){
+        this.waitingLobbies=new HashMap<>();
         this.lobbyPortNumber=lobbyPortNumber;
         this.serverController=ServerController.getIntance();
         try {
@@ -29,8 +32,17 @@ public class LobbyManager {
 
     }
 
+
+    /** the server listen for clients. One client aat time is listened. The client connects with a
+     * connection-message telling its nickname and its preferred game-mode.
+     * A buffer in reader is created.
+     * It is read the first message and it is checked the validity of the nickname(no other connected-players
+     * can have the same nickname)
+     * @throws IOException
+     */
     public void welcomeNewPlayers() throws IOException {
         JsonConverter jsonConverter=new JsonConverter();
+        ArrayList<String> usedNicknames= new ArrayList<>();
         ClientMessage firstMessage;
         while(true){
             Socket clientSocket = null;
@@ -50,9 +62,28 @@ public class LobbyManager {
             if(firstMessage.getHeader().getMessageType()=="ConnectionMessage"){
                 String nickname=firstMessage.getHeader().getNickname();
                 ConnectionMessage connectionMessage= (ConnectionMessage) firstMessage;
-                GameMode gameMode=connectionMessage.getGameMode();
-                addNickname(nickname, gameMode);
 
+                /**
+                 * Check of the nickname-univocity
+                 */
+                //check playing-clients
+                usedNicknames.clear();
+                for(int i=0; i<serverController.getGameControllers().size(); i++){
+                    for( Player p: serverController.getGameControllers().get(i).getGame().getPlayers() ){
+                        usedNicknames.add(p.getNickname());
+                    }
+                }
+                //check waiting-clients
+                for(GameMode gameMode: GameMode.values()){
+                    for(Lobby lobby: waitingLobbies.values() ){
+                        usedNicknames.addAll(lobby.getUsersReadyToPlay());
+                    }
+                }
+
+                if(!usedNicknames.contains(nickname)){
+                    GameMode gameMode=connectionMessage.getGameMode();
+                    addNickname(nickname, gameMode);
+                }
             }
         }
     }
