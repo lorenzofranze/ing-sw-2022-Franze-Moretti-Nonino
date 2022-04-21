cb433dc24358ae7afd0664d481f236cc430372a9
Valentina Moretti <valentina.moretti@mail.polimi.it>
2022-04-21 08:17:24 +0200

    Card12

--- src/main/java/it/polimi/ingsw/Server/Controller/Characters/Card12.java
+++ src/main/java/it/polimi/ingsw/Server/Controller/Characters/Card12.java
@@ -1,18 +1,33 @@
 package it.polimi.ingsw.Server.Controller.Characters;
 
 import it.polimi.ingsw.Server.Controller.GameController;
+import it.polimi.ingsw.Server.Controller.Network.MessageHandler;
 import it.polimi.ingsw.Server.Model.ColourPawn;
 import it.polimi.ingsw.Server.Model.Player;
 
 public class Card12 extends CharacterEffect{
     private ColourPawn colourPawn;
     private GameController gameController;
-    public Card12(GameController gameController, ColourPawn colourPawn){
-        this.colourPawn=colourPawn;
+    public Card12(GameController gameController){
+        this.gameController=gameController;
     }
 
 
     public void doEffect(){
+        boolean valid;
+        MessageHandler messageHandler = this.gameController.getMessageHandler();
+        int chosenPawn; // index of ColourPawn enumeration
+        do{
+            valid=false;
+            chosenPawn = messageHandler.getValueCLI("choose one color pawn: ",gameController.getCurrentPlayer());
+            for(ColourPawn p : ColourPawn.values()){
+                if(p.getIndexColour()==chosenPawn){
+                    valid=true;
+                }
+            }
+
+        }while(valid);
+
         for(Player p: gameController.getGame().getPlayers()){
             if(p.getSchoolBoard().getDiningRoom().get(colourPawn)>=3){
                 p.getSchoolBoard().getDiningRoom().remove(colourPawn,3);
