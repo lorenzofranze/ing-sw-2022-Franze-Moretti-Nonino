735ea3b241cb915719bafe782f7a524d0a8bd983
LaraNonino <lara.nonino@mail.polimi.it>
2022-05-11 22:20:22 +0200

    debugg

--- src/main/java/it/polimi/ingsw/common/messages/AsyncMessage.java
+++ src/main/java/it/polimi/ingsw/common/messages/AsyncMessage.java
@@ -1,18 +1,15 @@
 package it.polimi.ingsw.common.messages;
 
 public class AsyncMessage extends Message{
-    private TypeOfAsync typeOfAsync;
     private String description;
 
     public AsyncMessage(String description){
         super(TypeOfMessage.Async);
-        this.typeOfAsync = typeOfAsync;
         this.description = description;
     }
 
     public AsyncMessage(){
         super(TypeOfMessage.Async);
-        this.typeOfAsync = typeOfAsync;
         this.description = "";
     }
 
